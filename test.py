81cb063a22ebb8e166555bb55096ebc4454a2b09
Vel-Vladimir <vel.vlad.optimus@gmail.com>
2023-05-16 18:29:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-def print_func(f):
-    print(f.__name__)
+# print(0 and "22")
+# if True == 2:
+#     print("Inside If")
+# age = 12
+# adult = True if age >= 18 else False
+# print(adult)
 
-def my_function():
+# # Возьмем первый truly value
+# var = 0 or "" or -1
+# print("Var = ", var)
+print(1 or 0 and 0) # 1
+print((1 or 0) and 0) # 0
+
+while True:
+    int("a")
+    print("Inside while loop")
+    break
+else:
+    print("inside ELSE") # выпониться когда ни разу не выполнится цикл while или for
+
+try:
     pass
-
-print_func(my_function)
+except:
+    pass
+else:
+    pass # выпониться когда ни разу не выполнится исключение (exception)
