68600485fad6226b76fac3e42ac7179705740781
Vel-Vladimir <vel.vlad.optimus@gmail.com>
2023-05-16 18:44:07 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,17 +6,16 @@
 # print(adult)
 
 # # Возьмем первый truly value
-# var = 0 or "" or -1
-# print("Var = ", var)
+var = 0 or "" or -1
+print("Var = ", var)
 print(1 or 0 and 0) # 1
 print((1 or 0) and 0) # 0
 
 while True:
-    int("a")
     print("Inside while loop")
     break
 else:
-    print("inside ELSE") # выпониться когда ни разу не выполнится цикл while или for
+    print("inside ELSE in while loop") # выпониться когда ни разу не выполнится цикл while или for
 
 try:
     pass
